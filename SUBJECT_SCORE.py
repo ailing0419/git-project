326d5784642cdf59f9feb6d1f1e8fd02a827f229
ailing0419 <ailing2333@naver.com>
2023-04-21 19:43:48 +0900

    commit

--- SUBJECT_SCORE.py
+++ SUBJECT_SCORE.py
@@ -1,60 +1,52 @@
 
-TOTAL_GRADE = 0                     #열람용 학점 초기화
-TOTAL_GRADE2 = 0                    #제출용 학점 초기화
-SUBJECT_SCORE = 0                   #평점 환산 합계 초기화
-VALUE = 0                           #(교과목별 평점 * 학점)의 총합계 초기화
+#함수 정의
+def convert_score(grade):
+    match grade:
+        case 'A+':
+            gpa = 4.5
+        case 'A':
+            gpa = 4.0
+        case 'B+':
+            gpa = 3.5
+        case 'B':
+            gpa = 3.0
+        case 'C+':
+            gpa = 2.5
+        case 'C':
+            gpa = 2.0
+        case 'D+':
+            gpa = 1.5
+        case 'D':
+            gpa = 1.0
+        case 'F':
+            gpa = 0.0
+    return gpa
 
-answer = int(input("작업을 선택하세요.\n1.입력\n2.계산\n"))
+#입력
+archive_grade, submit_grade = 0, 0
+archive_gpa, submit_gpa = 0.0, 0.0
+while True:
+    user_value = int(input("\n\n작업을 선택하세요.\n1.입력\n2.계산\n"))
 
-#입력 작업 선택시 학점 및
-#평점 입력 ,저장,총합 계산후 변수에 저장
-#저장 끝난후 반복
-while answer == 1:
-    print("\n학점을 입력하세요:")
-    grade = int(input())
-    grade_2 = grade
-    TOTAL_GRADE += grade
-    print("평점을 입력하세요:")
-    subject_score = input()
+    if user_value == 1:
+        grade = int(input("\n\n학점을 입력하세요:"))
+        gpa = convert_score(input("평점을 입력하세요:"))
+        if gpa > 0:
+            submit_grade += grade
+            submit_gpa += grade * gpa
+        archive_grade += grade
+        archive_gpa += grade * gpa
 
-    if subject_score == 'A+':       #학점 구분 및 환산
-        score = 4.5
-    elif subject_score == 'A':
-        score = 4.0
-    elif subject_score == 'B+':
-        score = 3.5
-    elif subject_score == 'B':
-        score = 3.0
-    elif subject_score == 'C+':
-        score = 2.5
-    elif subject_score == 'C':
-        score = 2.0
-    elif subject_score == 'D+':
-        score = 1.5
-    elif subject_score == 'D':
-        score = 1.0
+        print("입력되었습니다.")
+   #출력
     else:
-        score = 0.0
-        grade_2 = 0
+        submit_gpa = round(submit_gpa / submit_grade, 2)
+        archive_gpa = round(archive_gpa / archive_grade, 2)
 
-    TOTAL_GRADE2 += grade_2
-    SUBJECT_SCORE += score
-    VALUE += grade * score
-    print("입력되었습니다.")
-    answer = int(input("\n\n작업을 선택 하세요.\n1.입력\n2.계산\n"))
-
-
-    #계산 작업 선택시 평균 GPA계산 후
-    # 출력 및 반복 정지
-    if answer == 2:
-        AVERAGE_SCORE = round(VALUE / TOTAL_GRADE, 2)               #열람용 GPA계산 및 소수점아래 2자리까지 보류
-        AVERAGE_SCORE2 = round(VALUE / TOTAL_GRADE2, 2)             #제출용 GPA계산 및 소수점아래 2자리까지 보류
-        print("\n제출용: " + str(TOTAL_GRADE2) + "학점 (GPA:" + str(AVERAGE_SCORE2) + ")")
-        print("열람용: " + str(TOTAL_GRADE) + "학점 (GPA:" + str(AVERAGE_SCORE) + ")")
+        print("\n제출용: " + str(submit_grade) + "학점 (GPA:" + str(submit_gpa) + ")")
+        print("열람용: " + str(archive_grade) + "학점 (GPA:" + str(archive_gpa) + ")")
         print("\n프로그램을 종료합니다.")
         break
-
-    continue
 
 
 
